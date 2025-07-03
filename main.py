50ac69c028834c80fdc2720249ee6894efb50af3
Igor Molchanov <141867556+TsipaPte@users.noreply.github.com>
2025-07-03 17:38:57 +0500

    Update main.py || Version 1.0.2
    Version 1.0.2
    
    * Added docstrings
    * Added main() function and check if __name__ == “__main__”
    * Other minor changes

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+""" Импортируем модуль decimal для точечных операций с дробными числами """
+
 import decimal
 
 class Calculator:
+    """ Главный класс для вычислений (калькулятор) """
     def __init__(self, expression: str) -> None:
+        """ Инициализатор атрибутов """
         self.exp = expression
 
     def calculate(self):
+        """ Основная функция для вычисления выражения """
         calc = self.exp.split()
 
         try:
@@ -36,8 +41,14 @@ class Calculator:
             return "[errno] Выражение должно быть в следующем формате: X Y Z где x и z - переменные, а y - оператор"
 
     def __str__(self):
+        """ Функция для отображения красивого вывода """
         return f"{self.calculate()}"
 
-userExpression = Calculator(input("Введите значение в следующем формате: x oper y: "))
+def main():
+    """ Главная функция main """
+    user_expression = Calculator(input("Введите значение в следующем формате: x oper y: "))
 
-print(userExpression)
+    print(user_expression)
+
+if __name__ == "__main__":
+    main()
